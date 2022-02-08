cfbbe867c6f2f7d49e9e187500a73280da2960ee
siny2000 <sundress198@gmail.com>
2022-02-08 18:49:26 +0700

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.css'
+import { Navbar,Nav} from "react-bootstrap";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar className="Navbar" bg="purple" variant="dark"
+      sticky="top" expand="sm" collapseOnSelect>
+      <Navbar.Brand>
+        CARGallery
+      </Navbar.Brand>
+    </Navbar>
+  </div>
   );
 }
 
